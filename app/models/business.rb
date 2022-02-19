03f777dd005c694c5776a15198776b46c81e1afe
Sudhanshu <sudhanshusiwan@gmail.com>
2022-02-19 12:29:44 +0100

    Add model validation and tests

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,8 +1,14 @@
 class Business < ApplicationRecord
-  validates :name, :email, :location, :telephone, :website, :instagram, :opens_at, :closes_at, :business_type_id, presence: true
-
   belongs_to :business_type
   has_one_attached :avatar
 
   acts_as_taggable_on :languages, :tags
+
+  validates :name, :email, :location, :website, :instagram, :opens_at, :closes_at, :business_type_id, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :website, :instagram, url: { no_local: true }
+  validates :telephone, :presence => true,
+                        :numericality => true,
+                        :length => { :minimum => 10, :maximum => 15 }
 end
