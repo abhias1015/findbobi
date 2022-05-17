b6d935bcbf831e1e0c09b6d220bf2b4f3ac7ef0a
Sudhanshu <sudhanshusiwan@gmail.com>
2022-05-17 22:08:52 +0200

    Upload CSV

--- app/admin/businesses.rb
+++ app/admin/businesses.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Business do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :email, :is_owner, :is_approved, :location, :telephone, :website, :instagram, :opens_at, :closes_at, :business_type_id
+  permit_params :name, :email, :is_owner, :is_approved, :tag_list, :language_list, :location, :telephone, :website, :instagram, :opens_at, :closes_at, :business_type_id
   #
   # or
   #
@@ -15,4 +15,43 @@ ActiveAdmin.register Business do
   #   permitted
   # end
 
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save("business", params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
+  form do |f|
+    f.inputs "Business info" do
+      f.input :business_type
+      f.input :name
+      f.input :email
+      f.input :is_owner
+      f.input :is_approved
+      f.input :location
+      f.input :telephone
+      f.input :website
+      f.input :instagram
+      f.input :opens_at
+      f.input :closes_at
+      f.input :tag_list
+      f.input :language_list
+      # f.input :tags,  # Show all tags AND checked already selected one (by relations through :tags - input must named :tags)
+      #   as: :select,
+      #   multiple: :true,
+      #   collection: ActsAsTaggableOn::Tag.select(:id, :name).all
+      # f.input :languages,  # Show all tags AND checked already selected one
+      #   as: :select,
+      #   multiple: :true,
+      #   collection: ActsAsTaggableOn::Tag.select(:id, :name).all
+    end
+    f.actions
+  end
 end
